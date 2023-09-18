6fd4f70f5ea31a3608d4637bf02deffb6b35aeaa
大笨峰 <1668959325@qq.com>
2023-09-18 11:54:11 +0800

    refacto: 修改目录结构

---
+++ src/Decorator/ReqDecorator/index.ts
@@ -0,0 +1,38 @@
+import "reflect-metadata"
+import {IData} from "../../index";
+import {  ReqReturnType} from "../../@types/ReqType";
+import {Get} from "../MethodDecorator/reqDecorator";
+import {ReqAop} from "../MethodAop/index"
+import {Params} from "../MethodDecorator/reqDecorator";
+import {BaseUrl} from "../ClassDecorator";
+
+
+
+const testBefore = () => {
+    console.log("ReqAop testBefore")
+}
+const testAfter = (responseData: any) => {
+    console.log(responseData.data[0].mission_name)
+}
+
+
+@BaseUrl('http://117.50.184.140:8844')
+class Clazz {
+
+    @Get('/user/queryReward')
+    async getReward(): ReqReturnType<IData<number>> {
+
+    }
+
+    @Params({date: '2023-08-29'})
+    @Get('/mission/getDayMissions')
+    @ReqAop({beforeCb: testBefore, afterCb: testAfter})
+    async getDayMissions(): ReqReturnType<IData<any>> {
+
+    }
+}
+
+new Clazz().getDayMissions().then((res) => {
+    console.log(res.data);
+})
+
